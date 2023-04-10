1a7014193fb2ee23166c2bc5fc966f9599770f24
r134x7 <r134x7@gmx.com>
2023-04-11 00:27:28 +1000

    manually changed sass files to css...

--- src/app/sample-a/sample-a.component.ts
+++ src/app/sample-a/sample-a.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sample-a',
   templateUrl: './sample-a.component.html',
-  styleUrls: ['./sample-a.component.sass']
+  styleUrls: ['./sample-a.component.css']
 })
 export class SampleAComponent {
 
