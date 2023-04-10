ab6560ed6b32017c2a08e13055a77b5f85313212
r134x7 <r134x7@gmx.com>
2023-04-11 01:40:02 +1000

    added class method, added mock heroes, applied ngFor and ngIf methods

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import type { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +8,10 @@ import type { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
+
+  heroes = HEROES;
 }
