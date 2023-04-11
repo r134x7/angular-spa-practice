68ac9c1ebd19438e446f40160531f946cc70b096
r134x7 <r134x7@gmx.com>
2023-04-11 17:30:27 +1000

    added message service injection

---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent {
+
+  // Angular only binds to public component properties. (to bind to the template)
+  constructor(public messageService: MessageService) {}
+}
